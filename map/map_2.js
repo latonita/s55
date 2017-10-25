b2bc2e43d8fc2dab6929e0c7fb665b471bb6cac5
latonita <a135768_s55@s23.h.mchost.ru>
2017-10-25 19:46:11 +0300

    Initial

---
+++ map/map_2.js
@@ -0,0 +1,38 @@
+var myMap;
+
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+
+
+function init () {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    myMap = new ymaps.Map('ya_map_pr', {
+        // При инициализации карты обязательно нужно указать
+        // её центр и коэффициент масштабирования.
+        center: [60.106841, 29.9652], // Объект
+        zoom: 12,
+	controls: ['zoomControl', 'fullscreenControl']
+		
+    });
+
+		
+       myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: ''
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'images/map-point.png',
+            // Размеры метки.
+            iconImageSize: [21, 21],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [0, 0]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+	myMap.behaviors.disable('scrollZoom');
+        	
+}
